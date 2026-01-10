3c21c77bd79b8ee0bdb0dac987ed1de0f77234da
nnaelmuna <naelmuna2009@gmail.com>
2026-01-10 18:36:14 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', serif;",
+        roboto: "'Roboto', serif;",
+      },
+      colors: {
+        primary: "#",
+        secondary: "",
+        textcolor: ""
+      }
+    },
+  },
+  plugins: [],
+}
+
